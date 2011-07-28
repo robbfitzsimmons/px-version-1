80fc39c91ca7ff2f042652c0fb8fb5a461cc1b41
Philip Dudley <pdudley89@gmail.com>
2011-07-28 03:48:40 -0400

    Test

--- models/user.rb
+++ models/user.rb
@@ -47,13 +47,13 @@ class User
     has_attached_file :image,
                     :storage => :s3,
                     #:s3_permissions => :public_read
-                    #:s3_credentials => "#{APP_ROOT}/config/s3.yml",
-                    :s3_credentials => {
-                      :access_key_id => ENV['S3_KEY'],
-                      :secret_access_key => 'test'
-                    },
+                    :s3_credentials => "#{APP_ROOT}/config/s3.yml",
+                    #:s3_credentials => {
+                     # :access_key_id => ENV['S3_KEY'],
+                      #:secret_access_key => ENV['S3_SECRET']
+                    #},
                     :path => "/:class/:attachment/:id/:style/:basename.:extension",
-                    :bucket         => "proximate_dev",
+                    :bucket         => "proximate_test",
                     :styles => { :original => "300x300#",
                                  :thumb => "80x80#" }
   elsif (ENV['RACK_ENV']) == "OOHHHH"
